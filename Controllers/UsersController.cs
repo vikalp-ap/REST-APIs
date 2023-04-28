d32afd98f03949a23d2d457b2e6ecd5525e8a04a
Vikalp Chakravorty <95289150+vikalp-ap@users.noreply.github.com>
2023-04-28 12:38:51 +0530

    added exception handling

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,115 +1,143 @@
+using System;
 using System.Data;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
 using RESTAPIs.Models;
 
-namespace RESTAPIs.Controllers;
-
-[ApiController]
-[Route("/[controller]")]
-
-public class UsersController : ControllerBase
+namespace RESTAPIs.Controllers
 {
-    private readonly IConfiguration _configuration;
-    public UsersController(IConfiguration configuration)
+    [ApiController]
+    [Route("/[controller]")]
+    public class UsersController : ControllerBase
     {
-        _configuration = configuration;
-    }
-
-    [HttpGet]
-    public JsonResult Get()
-    {
-        string query = @"SELECT * FROM users ORDER BY id ASC";
-        DataTable table = new DataTable();
-        string? psqlDataSource = _configuration.GetConnectionString("UsersCon");
-        NpgsqlDataReader myReader;
-        using (var conn = new NpgsqlConnection(psqlDataSource))
+        private readonly IConfiguration _configuration;
+        public UsersController(IConfiguration configuration)
         {
-            conn.Open();
-            using (NpgsqlCommand command = new NpgsqlCommand(query, conn))
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("/users")]
+        public IActionResult Get()
+        {
+            try
             {
-                myReader = command.ExecuteReader();
-                table.Load(myReader);
-                conn.Close();
+                string query = @"SELECT * FROM users ORDER BY id ASC";
+                DataTable table = new DataTable();
+                string? sqlDataSource = _configuration.GetConnectionString("UsersCon");
+                NpgsqlDataReader myReader;
+                using (var conn = new NpgsqlConnection(sqlDataSource))
+                {
+                    conn.Open();
+                    using (NpgsqlCommand command = new NpgsqlCommand(query, conn))
+                    {
+                        myReader = command.ExecuteReader();
+                        table.Load(myReader);
+                        conn.Close();
+                    }
+                }
+                return Ok(table);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
-        return new JsonResult(table);
-    }
 
-    [HttpPost]
-    [Route("/addUsers")]
-    public JsonResult Post(UsersModel user)
-    {
-        string query = @"INSERT INTO users (name, email, age) VALUES (@Name, @Email, @Age);";
-
-        DataTable table = new DataTable();
-        string? sqlDataSource = _configuration.GetConnectionString("UsersCon");
-        NpgsqlDataReader myReader;
-        using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+        [HttpPost]
+        [Route("/addUsers")]
+        public IActionResult Post(UsersModel user)
         {
-            myCon.Open();
-            using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+            try
             {
-                myCommand.Parameters.AddWithValue("@Name", user.Name);
-                myCommand.Parameters.AddWithValue("@Email", user.Email);
-                myCommand.Parameters.AddWithValue("@Age", user.Age);
-                myReader = myCommand.ExecuteReader();
-                table.Load(myReader);
+                string query = @"INSERT INTO users (name, email, age) VALUES (@Name, @Email, @Age);";
 
-                myReader.Close();
-                myCon.Close();
+                DataTable table = new DataTable();
+                string? sqlDataSource = _configuration.GetConnectionString("UsersCon");
+                NpgsqlDataReader myReader;
+                using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                    {
+                        myCommand.Parameters.AddWithValue("@Name", user.Name);
+                        myCommand.Parameters.AddWithValue("@Email", user.Email);
+                        myCommand.Parameters.AddWithValue("@Age", user.Age);
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
 
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                }
+                return Ok("Added Successfully");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
-        return new JsonResult("Added Successfully");
-    }
 
-    [HttpPut]
-    [Route("/updateUsers")]
-    public JsonResult Put(UsersModel user)
-    {
-        string query = @"UPDATE users SET name = @Name WHERE id = @Id;";
-        DataTable table = new DataTable();
-        string? sqlDataSource = _configuration.GetConnectionString("UsersCon");
-        NpgsqlDataReader myReader;
-        using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+        [HttpPut]
+        [Route("/updateUsers")]
+        public IActionResult Put(UsersModel user)
         {
-            myCon.Open();
-            using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+            try
             {
-                myCommand.Parameters.AddWithValue("@Name", user.Name);
-                myCommand.Parameters.AddWithValue("@Id", user.Id);
-                myReader = myCommand.ExecuteReader();
-                table.Load(myReader);
-                myReader.Close();
-                myCon.Close();
-
+                string query = @"UPDATE users SET name = @Name WHERE id = @Id;";
+                DataTable table = new DataTable();
+                string? sqlDataSource = _configuration.GetConnectionString("UsersCon");
+                NpgsqlDataReader myReader;
+                using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                    {
+                        myCommand.Parameters.AddWithValue("@Name", user.Name);
+                        myCommand.Parameters.AddWithValue("@Id", user.Id);
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                }
+                return Ok("Updated Successfully");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
-        return new JsonResult("Updated Successfully");
-    }
 
-    [HttpDelete]
-    [Route("/deleteUsers")]
-    public JsonResult Delete(UsersModel user)
-    {
-        string query = @"DELETE FROM users WHERE id = @Id;";
-        DataTable table = new DataTable();
-        string? sqlDataSource = _configuration.GetConnectionString("UsersCon");
-        NpgsqlDataReader myReader;
-        using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+        [HttpDelete]
+        [Route("/deleteUsers")]
+        public IActionResult Delete(UsersModel user)
         {
-            myCon.Open();
-            using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+            try
             {
-                myCommand.Parameters.AddWithValue("@Id", user.Id);
-                myReader = myCommand.ExecuteReader();
-                table.Load(myReader);
-                myReader.Close();
-                myCon.Close();
+                string query = @"DELETE FROM users WHERE id = @Id;";
+                DataTable table = new DataTable();
+                string? sqlDataSource = _configuration.GetConnectionString("UsersCon");
+                NpgsqlDataReader myReader;
+                using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                    {
+                        myCommand.Parameters.AddWithValue("@Id", user.Id);
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myCon.Close();
 
+                    }
+                }
+                return Ok("Updated Successfully");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
-        return new JsonResult("Deleted Successfully");
     }
 }
